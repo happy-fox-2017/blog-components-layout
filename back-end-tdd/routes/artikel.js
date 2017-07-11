43b1ca4c9ba12ee495cca41b5243602834839729
Antoni Angga <antoniangga14@gmail.com>
2017-07-11 20:22:48 +0700

    Running Well

---
+++ back-end-tdd/routes/artikel.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var cArtikel = require('../controller/cArtikel');
+
+/* GET home page. */
+router.get('/', cArtikel.artikelGet);
+router.post('/', cArtikel.artikelPost);
+router.put('/:id',cArtikel.artikelPut);
+router.get('/:id',cArtikel.artikelFindById);
+router.delete('/:id', cArtikel.artikelDelete);
+
+module.exports = router;
