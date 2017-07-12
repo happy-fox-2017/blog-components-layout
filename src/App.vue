804aa5d7fba788c5c4ff67e0612a424f6269da9f
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-12 09:24:47 +0700

    progress

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <div class="columns">
+      <div class="column is-3">
+        <sidebar></sidebar>
+      </div>
+      <div class="column is-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import ContentBar from '@/components/ContentBar'
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Sidebar,
+    ContentBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ src/components/MinifiedContent.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+<h1>Mini</h1>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      list_article: []
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
